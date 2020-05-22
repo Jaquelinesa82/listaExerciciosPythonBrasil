d0ad37bd02c672d5d2a87c6b8bc7c8161e368e25
jacke <jaquelinesa.82@gmail.com>
2020-05-22 15:36:17 -0300

    PEP8 - Lista 01

---
+++ 01_EstruturaSequencial/06_raio_area.py
@@ -0,0 +1,7 @@
+"""
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+"""
+import math
+
+raio = float(input('Digite o valor do raio: '))
+print('A área do circulo é {}'.format(2 * math.pi * raio))
