5a536b9dc004c4313dabfb5589b5e90871a9730c
jacke <jaquelinesa.82@gmail.com>
2020-05-22 17:18:13 -0300

    PEP8 - Lista 01

--- 01_EstruturaSequencial/11_inteiro_real_pow.py
+++ 01_EstruturaSequencial/11_inteiro_real_pow.py
@@ -4,8 +4,6 @@ o produto do dobro do primeiro mais metade do segundo .
 a soma do triplo do primeiro com o terceiro.
 o terceiro elevado ao cubo.
 """
-import math
-
 num1 = int(input('Digite um número inteiro: '))
 num2 = int(input('Digite outro número inteiro:'))
 num3 = float(input('Digite um número real: '))
@@ -13,5 +11,5 @@ prod = (2 * num1) + (2 / num2)
 soma = (3 * num1) + num3
 num3 = num3 ** 3
 
-print('produto é: {} e a soma: {}, o terceiro é: {}.'.format(prod, soma, num3))
-"""print('O terceiro elevado ao cubo é: {}'.format(num3))"""
+print('produto é: {} e a soma: {}, ao cubo é: {}.'.format(prod, soma, num3))
+
