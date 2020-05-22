9dc05a6b1fa971d2553353832ea1d5b5726391b2
jacke <jaquelinesa.82@gmail.com>
2020-05-22 15:49:02 -0300

    PEP8 - Lista 01

---
+++ 01_EstruturaSequencial/07_dobro_quadrado.py
@@ -0,0 +1,6 @@
+"""
+Faça um Programa que calcule a área de um quadrado,
+em seguida mostre o dobro desta área para o usuário.
+"""
+lado = int(input('Digite a área de um quadrado: '))
+print('O dobro da área é {} .'.format(2*(lado * lado)))
