ff5f59bd760759d53148facc950709ec45ed950e
jacke <jaquelinesa.82@gmail.com>
2020-05-22 21:22:56 -0300

    PEP8 - Lista 02

---
+++ 02_EstruturaDeDecisao/01_maior.py
@@ -0,0 +1,12 @@
+"""
+Faça um Programa que peça dois números e imprima o maior deles.
+"""
+num1 = int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+if (num1 > num2):
+    print(num1, 'Maior', num2)
+elif (num1 < num2):
+    print(num2, 'Maior', num1)
+else:
+    print('Números iguais')
