097a194b285eee5f151e17cf2b140d7d53ad3410
jacke <jaquelinesa.82@gmail.com>
2020-05-22 22:18:04 -0300

    PEP8 - Lista 02

---
+++ 02_EstruturaDeDecisao/04_vogal_consoante.py
@@ -0,0 +1,9 @@
+"""
+Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+"""
+letra = input('Digite um letra: ')
+
+if(letra == 'a','e','i','o','u'):
+    print('Vogal')
+else:
+    print('consoante')
